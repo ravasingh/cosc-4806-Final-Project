c78589a341ba8b10566127ac2a029ea9bba8d9ae
ravasingh <ravasingh@algomau.ca>
2024-07-21 18:47:25 +0000

    included a welcome message foe logged users about how users can search all movies and get its info in this website

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,6 +5,8 @@
             <div class="col-lg-12">
                 <h1>WELCOME</h1>
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
+                <p>Your ultimate destination for discovering, rating, and reviewing movies. Whether you're looking for the latest blockbusters or hidden gems, We got you covered.</p>
+                <p>Search for your favorite movies, read reviews from other movie enthusiasts, and share your own ratings and reviews to help others find their next great watch.</p>
 
             </div>
         </div>
